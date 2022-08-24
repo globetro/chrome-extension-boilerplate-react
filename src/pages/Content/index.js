23ec444ff8b7d8334c68382f760a92637fe4cadc
Warren Ouyang <warreno@gmail.com>
2022-08-24 10:47:29 -0700

    Remove unused permissions

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -7,18 +7,16 @@ printLine("Using the 'printLine' function from the Print Module");
 
 if (/.*\.reddit.com$/.test(window.location.host)) {
   chrome.storage.local.get(['clickedAt'], (result) => {
-    const lastClickedMs = Date.now() - result.clickedAt;
+    const lastClickedMs = Date.now() - (result.clickedAt || 0);
+    console.log('lastClicked', lastClickedMs);
     if (lastClickedMs > 5000) {
       document.documentElement.innerHTML = '';
       window.location.replace('about:blank');
-      console.log('getURL', chrome.runtime.getURL('newtab.html'));
     }
   });
 }
 
-document.addEventListener("click", function(e) {
+document.addEventListener('click', function(e) {
   console.log('click!', e);
-  window.__clickedTime = Date.now();
   chrome.storage.local.set({ clickedAt: Date.now() });
-  // Do what you want with click event
 }, false);
