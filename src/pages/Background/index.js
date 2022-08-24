7bf599c172eec4f532441658a9ad68dc1812458d
Warren Ouyang <warreno@gmail.com>
2022-08-24 10:14:01 -0700

    Block sites but allow if clicked through

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,27 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+console.log('!This is the background page.');
+console.log('! Put the background scripts here!');
+
+// var eventList = ['onBeforeNavigate', 'onCreatedNavigationTarget',
+//     'onCommitted', 'onCompleted', 'onDOMContentLoaded',
+//     'onErrorOccurred', 'onReferenceFragmentUpdated', 'onTabReplaced',
+//     'onHistoryStateUpdated'];
+
+// eventList.forEach(function(e) {
+//   chrome.webNavigation[e].addListener(function(data) {
+//     if (typeof data)
+//       console.log(chrome.i18n.getMessage('inHandler'), e, data);
+//     else
+//       console.error(chrome.i18n.getMessage('inHandlerError'), e);
+//   });
+// });
+
+// Reset the navigation state on startup. We only want to collect data within a
+// session.
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+//   console.log('!! tabId', tabId);
+//   console.log('!!! changeInfo', changeInfo);
+//   chrome.storage.local.get(['clickedAt'], (result) => {
+//     console.log('result', Date.now() - result.clickedAt);
+//   });
+// });
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,21 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+if (/.*\.reddit.com$/.test(window.location.host)) {
+  chrome.storage.local.get(['clickedAt'], (result) => {
+    const lastClickedMs = Date.now() - result.clickedAt;
+    if (lastClickedMs > 5000) {
+      document.documentElement.innerHTML = '';
+      window.location.replace('about:blank');
+      console.log('getURL', chrome.runtime.getURL('newtab.html'));
+    }
+  });
+}
+
+document.addEventListener("click", function(e) {
+  console.log('click!', e);
+  window.__clickedTime = Date.now();
+  chrome.storage.local.set({ clickedAt: Date.now() });
+  // Do what you want with click event
+}, false);
